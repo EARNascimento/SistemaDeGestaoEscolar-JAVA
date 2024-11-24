11c82524839a82659ecbde03a18a0387d36783e2
EARNascimento <eduardo90909@outlook.com>
2024-11-24 19:56:13 -0300

    Implementando a função do botão Cadastrar Curso

--- AppGestaoEscolarMatheusV2/src/main/java/com/sge/util/Curso.java
+++ AppGestaoEscolarMatheusV2/src/main/java/com/sge/util/Curso.java
@@ -1,8 +1,14 @@
 package com.sge.util;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Curso {
     private final String id;
     private String name;
+
+    private List<Disciplina> disciplinas = new ArrayList<>();
+    private List<Aluno> alunos = new ArrayList<>();
 
     public Curso(String id, String name){
         this.id = id;
@@ -24,17 +30,26 @@ public class Curso {
     }
 
     public void addAluno(Aluno aluno){
-
+        alunos.add(aluno);
     }
 
     public void removeAluno(Aluno aluno){
+        alunos.remove(aluno);
+    }
 
+    public void addDisciplina(Disciplina disciplina){
+        disciplinas.add(disciplina);
+    }
+
+    public void removeDisciplina(Disciplina disciplina){
+        disciplinas.remove(disciplina);
     }
 
     //Verificando o ID
     public boolean equals(Curso curso2){
         return this.id.equals(curso2.id);
     }
+
     //Retornando informações do Curso
     public String toString(){
         StringBuilder sb = new StringBuilder();
--- AppGestaoEscolarMatheusV2/src/main/java/com/sge/util/Utilitarios.java
+++ AppGestaoEscolarMatheusV2/src/main/java/com/sge/util/Utilitarios.java
@@ -1,4 +1,27 @@
 package com.sge.util;
 
+import javax.swing.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Utilitarios {
+
+    private List<Curso> cursos = new ArrayList<>();
+    private List<Aluno> alunos = new ArrayList<>();
+
+    public Utilitarios(){
+
+    }
+
+    public Curso setCurso(String id, String nome){
+        Curso novoCurso = new Curso(id, nome);
+        for(Curso curso : cursos){
+            if (curso.equals(novoCurso)){
+                JOptionPane.showMessageDialog(null, "Curso já existente");
+            }
+
+        }
+        cursos.add(novoCurso);
+        return novoCurso;
+    }
 }
