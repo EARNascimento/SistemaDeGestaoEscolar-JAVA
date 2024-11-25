eef6cbf170cf029c75b8cedcb8d5e68502fd17e2
EARNascimento <eduardo90909@outlook.com>
2024-11-25 08:35:23 -0300

    Testando o merge

--- AppGestaoEscolarMatheusV2/src/main/java/com/sge/main/Main.java
+++ AppGestaoEscolarMatheusV2/src/main/java/com/sge/main/Main.java
@@ -2,6 +2,7 @@ package com.sge.main;
 import com.sge.telas.LoginFrame;
 
 //Testando o merge
+//Testando o merge de novo, agora na main
 public class Main {
     public static void main(String[] args) {
         new LoginFrame().setVisible(true);
