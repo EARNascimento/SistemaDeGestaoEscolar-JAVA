2ead08b13e44183212bbd98a1a0bd4262847008c
EARNascimento <eduardo90909@outlook.com>
2024-11-25 08:30:41 -0300

    Testando a nova branch

--- AppGestaoEscolarMatheusV2/src/main/java/com/sge/main/Main.java
+++ AppGestaoEscolarMatheusV2/src/main/java/com/sge/main/Main.java
@@ -1,6 +1,7 @@
 package com.sge.main;
 import com.sge.telas.LoginFrame;
 
+//Testando o merge
 public class Main {
     public static void main(String[] args) {
         new LoginFrame().setVisible(true);
