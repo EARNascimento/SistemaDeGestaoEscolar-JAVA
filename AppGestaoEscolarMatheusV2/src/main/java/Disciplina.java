6fd92b317d4c009d75796b1cac5cce679118c408
EARNascimento <eduardo90909@outlook.com>
2024-11-26 17:56:53 -0300

    Preenchendo a classe Disciplina

--- AppGestaoEscolarMatheusV2/src/main/java/Disciplina.java
+++ AppGestaoEscolarMatheusV2/src/main/java/Disciplina.java
@@ -1,2 +1,29 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Disciplina {
+    private final String id;
+    private String nome;
+    private double nota;
+    private List<Curso> cursos = new ArrayList<>();
+
+    public Disciplina(String id, String nome){
+        this.id = id;
+        this.nome = nome;
+    }
+
+    //Getters
+    public String getId(){
+        return id;
+    }
+
+    public String getNome(){
+        return nome;
+    }
+
+    //Setters
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+
 }
